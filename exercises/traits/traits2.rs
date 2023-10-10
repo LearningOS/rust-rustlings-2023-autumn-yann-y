8ef8cbdf1395405753c560070b8c12b622cadde9
yann <yanzq@yzq.localdomain>
2023-10-10 11:08:33 +0800

    finish:traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,14 +8,20 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
-
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut arr = self.clone();
+        arr.insert(1,"Bar".to_string());
+        arr
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
